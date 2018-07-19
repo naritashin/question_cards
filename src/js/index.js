a9a5daf907bfdc838a468cf63f94f89c7316913f
Shin Narita <triplfjump1417@gmail.com>
2018-07-20 02:01:06 +0900

    includeできないためimportで対応

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
+import $ from 'jquery'
+import { parse } from 'url'
 import 'css/app'
+import Card from 'components/organisms/Card'
+import Navigation from 'components/organisms/Navigation'
+
+const urlHash = () => {
+  return parse(location.href).hash
+}
+
+console.log(urlHash())
+
+$('header').append(Navigation({ className: 'current' }))
+$('main').append(Card({ className: '' }))
