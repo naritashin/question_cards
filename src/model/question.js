c22bf42463bb2107bf2712ac18c6c9a1241def01
Shin Narita <triplfjump1417@gmail.com>
2018-07-20 22:13:51 +0900

    データ管理しやすくするため
    service, modelの追加

---
+++ src/model/question.js
@@ -0,0 +1,32 @@
+export default {
+  dianosis: {
+    dianosisTypeKey: 200,
+    dianosisAnswers: [
+      {
+        questionNumber: 1,
+        answer: 0,
+        unitType: '000000'
+      },
+      {
+        questionNumber: 2,
+        answer: 0,
+        unitType: '000000'
+      },
+      {
+        questionNumber: 3,
+        answer: 0,
+        unitType: '000100'
+      },
+      {
+        questionNumber: 4,
+        answer: 0,
+        unitType: '000200'
+      },
+      {
+        questionNumber: 5,
+        answer: 0,
+        unitType: '000000'
+      }
+    ]
+  }
+}
---
+++ src/service/questions.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    idName: 'first',
+    title: '年齢',
+    description: 'あなたに当てはまる年齢を選択してください。',
+    buttons: ['~25', '25~30', '21~35', '36~']
+  },
+  {
+    idName: 'second',
+    title: '性別',
+    description: 'あなたに当てはまる性別を選択してください。',
+    buttons: ['男', '女', 'その他', '答えたくない']
+  },
+  {
+    idName: 'third',
+    title: '身長',
+    description: 'あなたに当てはまる身長(cm)をせんたくしてください。',
+    buttons: ['~155', '156~165', '166~175', '176~']
+  },
+  {
+    idName: 'forth',
+    title: '体重',
+    description: 'あなたに当てはまる体重(kg)を選択してください。',
+    buttons: ['~45', '46~60', '61~75', '76~']
+  },
+  {
+    idName: 'fifth',
+    title: '睡眠時間',
+    description: 'あなたに当てはまる睡眠時間(h))を選択してください。',
+    buttons: ['~3', '4~6', '7~9', '10~']
+  }
+]
